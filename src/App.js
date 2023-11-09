d837e61b5b46b611e3814c63327478a053b26fcb
soomroshoaib <soomroshoaib178@gmail.com>
2023-11-09 03:37:13 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import {Routes, Route} from 'react-router-dom';
 import  './index.css'
 import './App.css';
+import Topbar from "./Screen/global/Topbar";
+import Dashboard from './Screen/dashboard';
+
 
 function App() {
   const [theme, colorMode] = useMode() 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <CssBaseline>
+        <CssBaseline />
     <div className="app">
-     <h1>soomro123 </h1>  
+     <mein className="content">
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+      </mein>  
     </div>
-    </CssBaseline>
+    
     </ThemeProvider>
     </ColorModeContext.Provider>
   );
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import {createContext, useState, useMemo} from 'react'
 import { createTheme } from '@mui/material/styles'
 
